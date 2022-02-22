e036bb65beae0b4d276b44a5e00807541e5e07fc
jakehowden <jakehowden@gmail.com>
2022-02-22 21:27:19 +0000

    Removed HTML and create a Next app to build the website in instead

---
+++ site/pages/jake.tsx
@@ -0,0 +1,12 @@
+import { FunctionComponent } from 'react'
+import styles from '../styles/Home.module.css'
+
+const Jake: FunctionComponent = () => {
+  return (
+    <div className={styles.container}>
+        Hello
+    </div>
+  )
+}
+
+export default Jake
